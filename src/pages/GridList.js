2d742f1b557118191603be22d108d9711a7942c9
SatyendraSinghDev <satyendrasinghdev2@gmail.com>
2023-05-15 23:36:04 +0530

    rough design left column

--- src/pages/GridList.js
+++ src/pages/GridList.js
@@ -2,7 +2,19 @@ import React from "react";
 import Grid from "../components/Grid";
 
 const GridList = () => {
-  return <Grid rows={3} columns={5} />;
+  // return <Grid rows={3} columns={5} />;
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ flex: "1", backgroundColor: "gray" }}>
+        <h6 className="mt-3 text-white">Create a Video Wall</h6>
+        <label forHtml="rows" className="text-white d-block">
+          Row
+        </label>
+        <input type="text" style={{ width: "50px", height: "40px" }} />
+      </div>
+      <div style={{ flex: "4", backgroundColor: "black" }}>Right Div</div>
+    </div>
+  );
 };
 
 export default GridList;
