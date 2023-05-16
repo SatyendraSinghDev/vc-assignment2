008506828914e9b33b4bfe4547f9e504ea7fbf02
SatyendraSinghDev <satyendrasinghdev2@gmail.com>
2023-05-16 15:09:04 +0530

    Create grid for video player

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { IoMdAddCircle } from "react-icons/io";
 
 const Grid = ({ rows, columns }) => {
-  return <>Grid Component</>;
+  const dummyArray = Array.from({ length: 20 }, (_, index) => index + 1);
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {dummyArray.map(() => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flex: "0 0 20%",
+            borderRight: "2px solid #3C4044",
+            borderBottom: "2px solid #3C4044",
+            height: "173px",
+          }}
+        >
+          <IoMdAddCircle size={26} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
