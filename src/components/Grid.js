ba137b0e5c8b26cab8f85bab5c9b791d4bec535c
SatyendraSinghDev <satyendrasinghdev2@gmail.com>
2023-05-16 20:57:52 +0530

    make grid dynamic

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,26 +2,43 @@ import React from "react";
 import { IoMdAddCircle } from "react-icons/io";
 
 const Grid = ({ rows, columns }) => {
-  const dummyArray = Array.from({ length: 20 }, (_, index) => index + 1);
+  const rowNumbers = Array.from({ length: rows }, (_, index) => index + 1);
+  const columnNumbers = Array.from(
+    { length: columns },
+    (_, index) => index + 1
+  );
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      {dummyArray.map(() => (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flex: "0 0 20%",
-            borderRight: "2px solid #3C4044",
-            borderBottom: "2px solid #3C4044",
-            height: "173px",
-          }}
-        >
-          <IoMdAddCircle size={26} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div style={{ flexWrap: "wrap" }}>
+        {rowNumbers?.map((row) => (
+          <div
+            key={row}
+            style={{
+              display: "flex",
+              // flexWrap: "wrap",
+            }}
+          >
+            {columnNumbers?.map((column) => (
+              <div
+                key={column}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flex: "0 0 20%",
+                  borderRight: "2px solid #3C4044",
+                  borderBottom: "2px solid #3C4044",
+                  height: "156px",
+                }}
+              >
+                <IoMdAddCircle />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/GridList.js
+++ src/pages/GridList.js
@@ -1,89 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsTable } from "react-icons/bs";
 import Grid from "../components/Grid";
 
 const GridList = () => {
-  // return <Grid rows={3} columns={5} />;
+  const [rows, setRows] = useState(null);
+  const [columns, setColumns] = useState(null);
+  const [inputRows, setInputRows] = useState(null);
+  const [inputColumns, setInputColumns] = useState(null);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    setRows(inputRows);
+    setColumns(inputColumns);
+  };
 
   return (
     <div style={{ display: "flex", height: "100vh" }}>
       <div style={{ flex: "1", backgroundColor: "#33373D" }}>
         <div style={{ padding: "20px 20px 20px 20px" }}>
           <h6 className="mt-3 text-white">Create a Video Wall</h6>
-          <div style={{ display: "flex", justifyContent: "space-around" }}>
-            <div>
-              <label forHtml="rows" className="text-white d-block">
-                Row
-              </label>
-              <input
-                type="text"
-                style={{
-                  width: "50px",
-                  height: "40px",
-                  backgroundColor: "#33373D",
-                  border: "1px solid #3C4044",
-                  color: "#FFFFFF",
-                  borderRadius: "5px",
-                  padding: "18px",
-                }}
-              />
+          <form onSubmit={handlerSubmit}>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <div>
+                <label htmlFor="rows" className="text-white d-block">
+                  Row
+                </label>
+                <input
+                  id="rows"
+                  type="text"
+                  style={{
+                    width: "50px",
+                    height: "40px",
+                    backgroundColor: "#33373D",
+                    border: "1px solid #3C4044",
+                    color: "#FFFFFF",
+                    borderRadius: "5px",
+                    padding: "18px",
+                  }}
+                  onChange={(e) => setInputRows(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="columns" className="text-white d-block">
+                  Column
+                </label>
+                <input
+                  id="columns"
+                  type="text"
+                  style={{
+                    width: "50px",
+                    height: "40px",
+                    backgroundColor: "#33373D",
+                    border: "1px solid #3C4044",
+                    color: "#FFFFFF",
+                    borderRadius: "5px",
+                    padding: "18px",
+                  }}
+                  onChange={(e) => setInputColumns(e.target.value)}
+                />
+              </div>
+              <div className="mt-4">
+                <button
+                  className="btn text-white"
+                  style={{
+                    backgroundColor: "#39548A",
+                    width: "70px",
+                    height: "40px",
+                  }}
+                  type="submit"
+                >
+                  Create
+                </button>
+              </div>
             </div>
-            <div>
-              <label forHtml="columns" className="text-white d-block">
-                Column
-              </label>
-              <input
-                id="columns"
-                type="text"
-                style={{
-                  width: "50px",
-                  height: "40px",
-                  backgroundColor: "#33373D",
-                  border: "1px solid #3C4044",
-                  color: "#FFFFFF",
-                  borderRadius: "5px",
-                  padding: "18px",
-                }}
-              />
-            </div>
-            <div className="mt-4">
-              <button
-                className="btn text-white"
-                style={{
-                  backgroundColor: "#39548A",
-                  width: "70px",
-                  height: "40px",
-                }}
-              >
-                Create
-              </button>
-            </div>
-          </div>
+          </form>
+
           <div
             className="d-flex justify-content-between"
             style={{ padding: "5px 0px 0px 5px" }}
           >
-            <div>
+            <div
+              onClick={() => {
+                setRows(2);
+                setColumns(3);
+              }}
+            >
               <BsTable className="text-white table-icon" />
               <p className="text-white">2x3</p>
             </div>
-            <div>
+            <div
+              onClick={() => {
+                setRows(3);
+                setColumns(4);
+              }}
+            >
               <BsTable className="text-white table-icon" />
-              <p className="text-white">2x3</p>
+              <p className="text-white">3x4</p>
             </div>
-            <div>
+            <div
+              onClick={() => {
+                setRows(4);
+                setColumns(5);
+              }}
+            >
               <BsTable className="text-white table-icon" />
-              <p className="text-white">2x3</p>
+              <p className="text-white">4x5</p>
             </div>
-            <div>
+            <div
+              onClick={() => {
+                setRows(5);
+                setColumns(6);
+              }}
+            >
               <BsTable className="text-white table-icon" />
-              <p className="text-white">2x3</p>
+              <p className="text-white">5x6</p>
             </div>
           </div>
         </div>
       </div>
       <div style={{ flex: "4", backgroundColor: "#060606" }}>
-        <Grid />
+        <Grid rows={rows} columns={columns} />
       </div>
     </div>
   );
